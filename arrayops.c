a605ec788f29f6c7666aca934933d8564ef581a4
emilio-diaz <emilio@y7mail.com>
2023-12-28 11:47:31 +1000

    Added new library for Array Operations 'arrayops'
    - reverse: flips array by swapping each index with its corresponding index from the end of the array.
    Eg. Length = 8
        array[0] <> array[7]
        array[1] <> array[6]
        array[2] <> array[5]
        array[3] <> array[4]
    
    If length is odd, the middle index value remains the same

---
+++ arrayops.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "arrayops.h"
+
+#define DEBUG 0
+
+int reverse(int* arr, int size){
+    char temp;
+    int start = 0;
+    int end = (size-1);
+
+    if(size < 1){
+        perror("Invalid array size...\n");
+        return -1;
+    }
+    else if(size == 1){
+        perror("No need to reverse this array of length 1...\n");
+        return -2;
+    }
+
+    if(DEBUG){
+        printf("\n");
+        printf("Normal array -> ");
+        for(int i=0; i<size; i++){
+            printf("%d", arr[i]);
+        }
+        printf("\n");
+    }
+
+    while(start < end) {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+
+    if(DEBUG){
+        printf("\n");
+        printf("Reversed array -> ");
+        for(int i=0; i<size; i++){
+            printf("%d", arr[i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ arrayops.h
@@ -0,0 +1,6 @@
+#ifndef ARRAYOPS
+#define ARRAYOPS
+
+int reverse(int* arr, int size);
+
+#endif
